ab40e09b4419f21dc898edc8a08b89e63981126e
remcopoelstra <remco@poelstrabesturingen.nl>
2023-11-30 13:42:54 +0100

    Added date format for showing date with day and month without year

--- common-locale/src/main/java/org/glasspath/common/format/FormatUtils.java
+++ common-locale/src/main/java/org/glasspath/common/format/FormatUtils.java
@@ -44,6 +44,7 @@ public class FormatUtils {
 	public static final DateFormat DATE_FORMAT_YEAR = new SimpleDateFormat("yyyy");
 	public static final DateFormat DATE_FORMAT_MONTH = new SimpleDateFormat("MMM-yyyy");
 	public static final DateFormat DATE_FORMAT_DAY = new SimpleDateFormat("E");
+	public static final DateFormat DATE_FORMAT_DAY_MONTH = new SimpleDateFormat("E d MMM");
 	public static final DateFormat ANDROID_DATE_FORMAT = new SimpleDateFormat("EEEE d MMMM yyyy");
 	static {
 		DATE_FORMAT.setTimeZone(DateUtils.TIME_ZONE);
