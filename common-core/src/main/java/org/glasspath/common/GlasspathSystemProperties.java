59dd0578441c131c1c1c693109935ed743c09345
remcopoelstra <remco@poelstrabesturingen.nl>
2024-07-02 09:58:06 +0200

    Added BUNDLED_MODELS_PATH system property

--- common-core/src/main/java/org/glasspath/common/GlasspathSystemProperties.java
+++ common-core/src/main/java/org/glasspath/common/GlasspathSystemProperties.java
@@ -29,6 +29,6 @@ public interface GlasspathSystemProperties {
 
 	String BUNDLED_FONTS_PATH = "glasspath.bundledFontsPath";
 
-	String MODELS_PATH = "glasspath.modelsPath";
+	String BUNDLED_MODELS_PATH = "glasspath.bundledModelsPath";
 
 }
